a3f673d5219723443b7a849f9631483b5bff390a
chaoshandiaomao <128349784+chaoshandiaomao@users.noreply.github.com>
2023-06-03 20:24:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,4 +15,5 @@
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 #sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd
+#sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
